e38d72569f30f9b8dd686399ea262d43ed79f8ab
Brandon Jernigan <bjernigan@netspend.com>
2018-05-30 15:46:44 -0500

    updated home with api call, redux integration

--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { arrayOf, func, object } from 'prop-types';
 
-import { Heading } from '../components';
+import { SET_DATA } from '../actions';
+import { Heading, List } from '../components';
 
-export default class Home extends Component {
+export class Home extends Component {
+
+  static propTypes = {
+    data: arrayOf(object),
+    setData: func
+  }
+
+  static defaultProps = {
+    data: [],
+    setData: () => {}
+  }
+
+  async componentDidMount() {
+    const data = await this.getData();
+    this.props.setData(data);
+  }
+
+  async getData() {
+    return await fetch('http://localhost:3000/menu').then(res => res.json()).catch(err => ({ err }));
+  }
 
   render() {
+    const {
+      data
+    } = this.props;
     return (
       <div>
         <Heading>test</Heading>
+        <List items={data} />
       </div>
     );
   }
 
 }
+
+export const mapStateToProps = (state) => {
+  const {
+    data
+  } = state;
+  return {
+    data
+  };
+}
+
+export const mapDispatchToProps = (dispatch) => {
+  return {
+    setData: (data) => {
+      dispatch({
+        type: SET_DATA,
+        data
+      });
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/containers/home.test.jsx
+++ src/containers/home.test.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Home from './home';
+
+import { SET_DATA } from '../actions';
+import {
+  Home,
+  mapStateToProps,
+  mapDispatchToProps
+} from './home';
 
 let home;
 
 beforeEach(() => {
+  global.fetch = jest.fn().mockImplementation(() => {
+    const p = new Promise((resolve) => { // eslint-disable-line no-undef
+      resolve([{
+        text: 'Test'
+      }]);
+    });
+
+    return p;
+  })
+  Home.getItems = jest.fn();
   home = mount(<Home />);
 });
 
@@ -14,5 +30,31 @@ describe('<Home /> Container', () => {
   });
   it('renders the Heading', () => {
     expect(home.find('h1').length).toEqual(1);
+  });
+});
+
+describe('mapStateToProps', () => {
+  it('returns data', () => {
+    const props = mapStateToProps({ data: [1] });
+    expect(props).toEqual({ data: [1] });
+  });
+});
+
+describe('mapDispatchToProps', () => {
+  it('returns setData prop', () => {
+    const props = mapDispatchToProps(() => {});
+    expect(props.setData).toBeDefined();
+  });
+  it('should call SET_DATA when calling setData', () => {
+    const dispatch = jest.fn();
+    mapDispatchToProps(dispatch).setData([{
+      text: 'Test'
+    }]);
+    expect(dispatch.mock.calls[0][0]).toEqual({
+      data: [{
+        text: 'Test'
+      }],
+      type: SET_DATA
+    });
   });
 });
